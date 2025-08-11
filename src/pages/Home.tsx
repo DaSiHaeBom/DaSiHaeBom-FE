59fb24db8170847aa77ce7d270c18ae6f6ab067a
Kwon <kdg5436@gmail.com>
2025-08-11 21:20:47 +0900

    feat: 이력서 작성 버튼 상태 관리 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
+import { useState } from 'react';
 import HomeImage from '../assets/HomeAssets/homeImage.svg';
 import { HomeCard } from '../components/Home/HomeCard';
 
 const Home = () => {
+  const [isResumeCreated, setIsResumeCreated] = useState(false);
   return (
     <div className="h-screen flex flex-col items-center justify-center gap-30 mt-16">
-      {/* body */}
-      <div className="flex flex-col items-center justify-center mt-16">
+      <div className="flex flex-col items-center justify-center mt-30">
         <p className="text-center text-neutral-700 text-4xl font-bold leading-16">
           이력서를 쓰면, 기업이 먼저 연락합니다.
         </p>
@@ -17,9 +18,21 @@ const Home = () => {
         <p className="text-neutral-700 text-lg font-medium leading-loose mb-10">
           아직 이력서가 없어요. 먼저 이력서를 작성해 주세요.
         </p>
-        <button className="w-40 h-14 rounded-xl bg-[#FF6B01] text-white text-xl font-semibold px-4 py-2 leading-loose">
-          이력서 작성
-        </button>
+        {isResumeCreated ? (
+          <button
+            className="w-40 h-14 rounded-lg bg-white text-[#FF6B01] border border-[#FF6B01] text-xl font-semibold px-4 py-2 leading-loose"
+            onClick={() => setIsResumeCreated(false)}
+          >
+            이력서 보기
+          </button>
+        ) : (
+          <button
+            className="w-40 h-14 rounded-xl bg-[#FF6B01] text-white text-xl font-semibold px-4 py-2 leading-loose"
+            onClick={() => setIsResumeCreated(true)}
+          >
+            이력서 작성
+          </button>
+        )}
       </div>
       <div className="flex flex-col justify-center items-center gap-10">
         <p className="text-neutral-700 text-4xl font-bold leading-16">
