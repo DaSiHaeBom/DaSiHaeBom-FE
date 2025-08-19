34e0eb7adf45639507903945049e377c30e64672
SonYoungJun <dudwnsths1@naver.com>
2025-08-19 22:12:33 +0900

    feat(routes): 이력서 리스트 페이지 라우팅 추가

--- src/utils/routes.tsx
+++ src/utils/routes.tsx
@@ -6,6 +6,7 @@ import BusinessMyPage from '../pages/BusinessMyPage';
 import BusinessInfoPage from '../pages/BusinessInfoPage';
 import PersonalMyPage from '../pages/PersonalMyPage';
 import PersonalInfoPage from '../pages/PersonalInfoPage';
+import ResumeListPage from '../pages/ResumeListPage';
 
 const routes = createBrowserRouter([
   {
@@ -35,6 +36,10 @@ const routes = createBrowserRouter([
           { index: true, element: <BusinessMyPage /> }, // /business/mypage
           { path: 'profile', element: <BusinessInfoPage /> }, // /business/mypage/profile
         ],
+      },
+      {
+        path: 'resume/list',
+        element: <ResumeListPage />,
       },
     ],
   },
