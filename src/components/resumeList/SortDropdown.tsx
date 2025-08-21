4ee37feb71e167bedb10887120f8155257292974
SonYoungJun <dudwnsths1@naver.com>
2025-08-22 00:27:34 +0900

    styles: 정렬 드롭다운 디자인 수정

--- src/components/resumeList/SortDropdown.tsx
+++ src/components/resumeList/SortDropdown.tsx
@@ -22,7 +22,7 @@ const SortDropdown = ({ sort, setSort }: Props) => {
       {/* 드롭다운 열기 버튼 */}
       <button
         onClick={() => setOpen(!open)}
-        className="h-[50px] w-[136px] px-3 rounded-[8px] border border-[#D9D9D9] bg-[#FFFEFD] text-[#3C3C3C] flex justify-around items-center text-[20px] font-semibold hover:bg-gray-50"
+        className="h-[50px] w-full min-w-[136px] px-3 rounded-[10px] border border-[#D9D9D9] bg-[#FFFEFD] text-[#3C3C3C] flex justify-center gap-3 items-center text-[20px] font-semibold hover:bg-gray-50"
       >
         {sort === 'latest' ? '최신순' : '거리순'}
         <img
@@ -34,21 +34,20 @@ const SortDropdown = ({ sort, setSort }: Props) => {
 
       {/* 열렸을 때만 메뉴 보여주기 */}
       {open && (
-        <div className="absolute left-0 mt-2 w-[160px] rounded-[10px] border border-[#E7E5E4] bg-white shadow">
+        <div className="absolute left-0 mt-1 w-[136px] rounded-[10px] border border-[#E7E5E4] bg-white shadow overflow-hidden">
           <button
             onClick={() => handleSelect('latest')}
-            className={`w-full text-left px-3 py-2 text-[14px] ${
-              sort === 'latest' ? 'bg-gray-100 font-medium' : 'hover:bg-gray-50'
+            className={`w-full font-semibold text-[#3c3c3c] text-left px-6 py-2 text-[20px] hover:bg-gray-100 ${
+              sort === 'latest' ? 'bg-gray-100' : ''
             }`}
           >
             최신순
           </button>
+          <div className="h-[1px] bg-[#E7E5E4]" />
           <button
             onClick={() => handleSelect('distance')}
-            className={`w-full text-left px-3 py-2 text-[14px] ${
-              sort === 'distance'
-                ? 'bg-gray-100 font-medium'
-                : 'hover:bg-gray-50'
+            className={`w-full font-semibold text-[#3c3c3c] text-left px-6 py-2 text-[20px] hover:bg-gray-100 ${
+              sort === 'distance' ? 'bg-gray-100' : ''
             }`}
           >
             거리순
