51d1f1a8d1297801418ac5ae91086fc4d6121ec5
UHyeonj <dngusdlqwkd@gmail.com>
2025-08-25 16:37:22 +0900

    fix: 텍스트 중앙 정렬

--- src/components/Home/HomeCard.tsx
+++ src/components/Home/HomeCard.tsx
@@ -15,7 +15,7 @@ export const HomeCard = ({
         {step}
       </p>
       <img src={imageSrc} alt={altText} />
-      <p className="text-neutral-700 text-2xl font-semibold leading-9 whitespace-pre-line">
+      <p className="text-neutral-700 text-center text-2xl font-semibold leading-9 whitespace-pre-line">
         {text}
       </p>
     </div>
